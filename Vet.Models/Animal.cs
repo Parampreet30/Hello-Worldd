0a9ea252e4f779800417fc64957e8560718b3246
Parampreet30 <A00264414@mycambrian.ca>
2024-11-21 00:06:57 +0000

    constructor added

--- Vet.Models/Animal.cs
+++ Vet.Models/Animal.cs
@@ -2,5 +2,17 @@
 
 public class Animal
 {
+ public string Name { get; set; }
+  public string Owner { get; set; }
+  public decimal Height { get; set; }
 
+  public decimal Weight { get; set; }
+
+  public Animal(string name, string owner, decimal height, decimal height)
+  {
+    this.Name = name;
+    this.Owner = owner;
+    this.Height = height;
+    this.Weight = Weight;
+  }
 }
